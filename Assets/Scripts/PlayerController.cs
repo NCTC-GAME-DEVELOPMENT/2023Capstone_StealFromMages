10e24dbfcd0092c41f875a8e3afcf60d9bfd77fd
Grash <grashuriza@gmail.com>
2023-10-17 18:34:00 -0500

    Input and Test Player
    Input Poller and Data classes added
    Input Poller Removed NoDestryoOnLoad call removed.
    Test Player Created, have example movement, facing, right trigger attack.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int playerNumber = 1;
+    public float moveSpeed = 4f;
+    public float triggerActAt = .9f; 
+    
+    InputData input; 
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = InputPoller.reference.GetInput(playerNumber);
+        rb.velocity = Vector2.zero;
+
+        MovePlayer(input.leftStick);
+        RotatePlayer(input.rightStick); 
+
+        if (input.rightTrigger > triggerActAt)
+        {
+            Debug.Log("Fire"); 
+        }    
+        
+    }
+
+    void MovePlayer (Vector2 value)
+    {
+        rb.velocity = value * moveSpeed;
+    }
+
+    void RotatePlayer(Vector2 value)
+    {
+        gameObject.transform.right = value; 
+    }
+}
