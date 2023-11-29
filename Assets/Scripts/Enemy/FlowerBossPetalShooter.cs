ad173b85bbd4f2a467432df0e024779f098e91db
Trevor Rose <trevorrose@outlook.com>
2023-11-28 21:31:56 -0600

    Added SOME Flower Boss Functionality
    Mainly getting the rhythm  of attacks down

---
+++ Assets/Scripts/Enemy/FlowerBossPetalShooter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBossPetalShooter : MonoBehaviour
+{
+    public void Shoot() {
+        Debug.Log(this.gameObject.name + " Shoots");
+    }
+}
